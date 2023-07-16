7a42781dfb5449ffd5ee05fc48420133b37ab74d
Krystian731 <krystianolejniczak731@tlen.pl>
2023-07-16 13:41:43 +0200

    [GAS-10][feat] add animations for mobile

--- src/app/facilities/facilities.component.ts
+++ src/app/facilities/facilities.component.ts
@@ -12,8 +12,20 @@ import {fadeSlideInOutAnimation} from "../shared/animations";
 export class FacilitiesComponent implements OnInit{
     public descriptionToDisplay?: string;
 
+    private observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.2 });
+
     ngOnInit(){
         this.descriptionToDisplay = 'first';
+
+        document.querySelectorAll('app-facility-descripton-mobile').forEach((element) => {
+            this.observer.observe(element);
+        });
     }
 
     setDescriptionToDisplay(descriptionName: string){
