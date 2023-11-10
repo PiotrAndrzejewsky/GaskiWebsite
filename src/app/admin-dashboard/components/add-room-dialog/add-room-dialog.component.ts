a03b786002723cbaf9c1274696855eb464bc3210
Krystian731 <krystianolejniczak731@tlen.pl>
2023-11-10 16:43:29 +0100

    [develop][feat] add pushing new room to server

--- src/app/admin-dashboard/components/add-room-dialog/add-room-dialog.component.ts
+++ src/app/admin-dashboard/components/add-room-dialog/add-room-dialog.component.ts
@@ -1,35 +1,51 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
+import {Room} from "../../../core/models/room.model";
 
 @Component({
-  selector: 'app-add-room-dialog',
-  templateUrl: './add-room-dialog.component.html',
-  styleUrls: ['./add-room-dialog.component.scss']
+    selector: 'app-add-room-dialog',
+    templateUrl: './add-room-dialog.component.html',
+    styleUrls: ['./add-room-dialog.component.scss']
 })
 export class AddRoomDialogComponent {
 
     public roomForm: FormGroup;
+
     constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<AddRoomDialogComponent>) {
         this.roomForm = this.fb.group({
-        title: ['', Validators.required],
-        shortDescription: ['', Validators.required],
-        longDescription: ['', Validators.required],
-        maximumNumberOfPeopleAllowed: ['', Validators.required],
-        basePricePerNight: ['', Validators.required],
-        pricePerPerson: ['', Validators.required],
-        isPlaceReservable: [false],
-        imageLink: ['', Validators.required],
-    });}
+            title: ['', Validators.required],
+            shortDescription: ['', Validators.required],
+            longDescription: ['', Validators.required],
+            maximumNumberOfPeopleAllowed: ['', Validators.required],
+            basePricePerNight: ['', Validators.required],
+            pricePerPerson: ['', Validators.required],
+            isPlaceReservable: [false],
+            imageLink: ['', Validators.required],
+        });
+    }
 
 
     onSubmit() {
-        // Handle the form submission here
-        // You can access form values using this.roomForm.value
-        console.log(this?.roomForm?.value);
+
+        if (this.roomForm.invalid)
+            return
+
+        let newRoom: Room = {
+            title: this.roomForm.value.title,
+            shortDescription: this.roomForm.value.shortDescription,
+            longDescription: this.roomForm.value.longDescription,
+            maximumNumberOfPeopleAllowed: this.roomForm.value.maximumNumberOfPeopleAllowed,
+            basePricePerNight: this.roomForm.value.basePricePerNight,
+            pricePerPerson: this.roomForm.value.pricePerPerson,
+            isPlaceReservable: this.roomForm.value.isPlaceReservable,
+            imageLink: this.roomForm.value.imageLink, // this will be file list object
+        };
+
+        this.dialogRef.close(newRoom);
     }
 
-    onCancel(): void {
+    onCancel() {
         this.dialogRef.close();
     }
 }
