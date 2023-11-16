fb1df9cad021a9c09248c1ee9e5950aafaf77eeb
Krystian731 <krystianolejniczak731@tlen.pl>
2023-11-16 12:10:10 +0100

    [develop][style] style admin dashboards

--- src/app/admin-dashboard/components/add-facilitie-dialog/add-facilitie-dialog.component.ts
+++ src/app/admin-dashboard/components/add-facilitie-dialog/add-facilitie-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import {Facilitie} from "../../../core/models/facilitie.model";
@@ -8,19 +8,16 @@ import {Facilitie} from "../../../core/models/facilitie.model";
     templateUrl: './add-facilitie-dialog.component.html',
     styleUrls: ['./add-facilitie-dialog.component.scss']
 })
-export class AddFacilitieDialogComponent {
-    facilityForm: FormGroup;
-
-    constructor(
-        public dialogRef: MatDialogRef<AddFacilitieDialogComponent>,
-        private fb: FormBuilder
-    ) {
-        this.facilityForm = this.fb.group({
+export class AddFacilitieDialogComponent  {
+    facilityForm: FormGroup = this.fb.group({
             title: ['', [Validators.required]],
             description: ['', [Validators.required]],
             src: ['', [Validators.required]]
         });
-    }
+
+    constructor( public dialogRef: MatDialogRef<AddFacilitieDialogComponent>, private fb: FormBuilder) {}
+
+
 
     onCancel(): void {
         this.dialogRef.close();
